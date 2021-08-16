bd31cab9541f014f0edb39959089cad6321982f0
dushaobin <dushaobin@liepin.com>
2021-08-16 15:16:21 +0800

    feat: support custom filter

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,20 @@ import { Plugin } from 'esbuild';
 import less from 'less';
 import { convertLessError, getLessImports } from './less-utils';
 
+export interface LoaderOptions {
+  /* custom filter */
+  filter?: RegExp,
+}
+
 /** Less-loader for esbuild */
-export const lessLoader = (options: Less.Options = {}): Plugin => {
+export const lessLoader = (options: Less.Options = {}, loaderOptions: LoaderOptions = {}): Plugin => {
   return {
     name: 'less-loader',
     setup: (build) => {
+      const filter = loaderOptions.filter;
       // Resolve *.less files with namespace
-      build.onResolve({ filter: /\.less$/, namespace: 'file' }, (args) => {
+      build.onResolve({ filter: filter || /\.less$/, namespace: 'file' }, (args) => {
         const filePath = path.resolve(process.cwd(), path.relative(process.cwd(), args.resolveDir), args.path);
-
         return {
           path: filePath,
           watchFiles: !!build.initialOptions.watch ? [filePath, ...getLessImports(filePath)] : undefined,
@@ -20,11 +25,10 @@ export const lessLoader = (options: Less.Options = {}): Plugin => {
       });
 
       // Build .less files
-      build.onLoad({ filter: /\.less$/, namespace: 'file' }, async (args) => {
+      build.onLoad({ filter: filter || /\.less$/, namespace: 'file' }, async (args) => {
         const content = await fs.readFile(args.path, 'utf-8');
         const dir = path.dirname(args.path);
         const filename = path.basename(args.path);
-
         try {
           const result = await less.render(content, {
             filename,
